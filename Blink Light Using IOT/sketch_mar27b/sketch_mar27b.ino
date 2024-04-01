158ff2d4d1306a10aa1a74686aae506463ddd2ca
Suyash Saxena <99425562+Suyashspidy@users.noreply.github.com>
2024-04-01 04:07:56 -0700

    Add files via upload

---
+++ Blink Light Using IOT/sketch_mar27b/sketch_mar27b.ino
@@ -0,0 +1,40 @@
+char input[12]; 
+int count = 0;
+
+// SETUP FUNCTION
+
+void setup()
+
+{
+
+Serial.begin(9600); // START SERIAL AT BAUD RATE OF 9600 BITS/SEC
+
+}
+
+
+void loop()
+{
+
+if(Serial.available()) // CHECK FOR AVAILABILITY OF SERIAL DATA
+
+{ count = 0; // Reset the counter to zero
+
+while(Serial.available() && count < 12) 
+{
+
+input[count] = Serial.read(); // Read 1 Byte of data and store it in the input[] variable
+
+count++; // increment counter delay(5); 
+}
+
+// PRINTING RFID TAG
+
+for(int i=0;i<12;i++)
+
+Serial.print(input[i]);
+
+Serial.println();
+
+}
+
+}
